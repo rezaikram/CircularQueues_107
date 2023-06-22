220144b7f59c08c45d8ff75e2ae18641adbab787
rezaikram <muhammadikramriza@gmail.com>
2023-06-22 15:49:25 +0700

    menampilkan hasil

--- CircularQueves_107/CircularQueves_107.cpp
+++ CircularQueves_107/CircularQueves_107.cpp
@@ -91,3 +91,49 @@ public:
 	}
 };
 
+int main() {
+	Queues q;
+	char ch;
+
+	while (true) {
+		try {
+			cout << "Menu" << endl;
+			cout << "1. Implement insert operation" << endl;
+			cout << "2. Implement delete operation" << endl;
+			cout << "3. display values" << endl;
+			cout << "4. exit" << endl;
+			cout << "Enter your choice (1-4):";
+			cin >> ch;
+			cout << endl;
+
+			switch (ch) {
+			case '1': {
+
+				q.insert();
+				break;
+			}
+			case'2': {
+				q.remove();
+				break;
+			}
+			case'3': {
+				q.display();
+				break;
+			}
+			case'4': {
+
+				return 0;
+			}
+			default: {
+				cout << "invalid option!!" << endl;
+				break;
+			}
+			}
+		}
+		catch (exception& e) {
+			cout << "check for the values entered." << endl;
+		}
+	}
+
+	return 0;
+}
