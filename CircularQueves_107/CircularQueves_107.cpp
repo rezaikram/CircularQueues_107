a55e8eca1b668f78b83afb1870ff10319b8bb835
rezaikram <muhammadikramriza@gmail.com>
2023-06-22 15:39:48 +0700

    menghapus element

--- CircularQueves_107/CircularQueves_107.cpp
+++ CircularQueves_107/CircularQueves_107.cpp
@@ -39,4 +39,26 @@ public:
 		queue_array[REAR] = num;
 	}
 
+	void remove() {
+		//cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "Queue inderflow\n";
+			return;
+		}
+		cout << "\nThe element delected from the queue is:" << queue_array[FRONT] << "\n";
+
+		//cek jika antrian hanya memiliki satu elemen
+		if (FRONT == REAR) {
+			FRONT = -1;
+			REAR = -1;
+		}
+		else {
+			//jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+			if (FRONT == max - 1)
+				FRONT == 0;
+			else
+				FRONT = FRONT + 1;
+		}
+	}
+
 	
